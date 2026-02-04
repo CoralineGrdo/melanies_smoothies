7f818f9db3044fa688c1f2fda1287e9d87fb891c
CoralineGrdo <coraline.garrido@larochellegc.com>
2026-02-04 09:13:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,11 +53,14 @@ if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"✅ Your Smoothie is ordered, {name_on_order.strip()}!")
 
-        # Call SmoothieFroot once for each chosen fruit
+        # Show SmoothieFroot nutrition for each chosen fruit
         for fruit_chosen in ingredients_list:
+            st.subheader(f"{fruit_chosen} Nutrition Information")
+
             smoothiefroot_response = requests.get(
-                "https://my.smoothiefroot.com/api/fruit/watermelon"
+                "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
             )
+
             sf_df = st.dataframe(
                 data=smoothiefroot_response.json(),
                 use_container_width=True
